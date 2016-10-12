8d8c19291a7609d70714ba7f5ed742ff564fbd1a
Joe Ebmeier <joe.ebmeier@firespring.com>
2016-10-12 12:52:09 -0500

    Updated run.sh to read it's variables from the environment

--- run.sh
+++ run.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -x
 
-domain=$1
-classes=$2
+DOMAIN_NAME=$1
+CLASS_NAMES=$2
 
-node=$3
-if [ "`echo $node`" == "" ]
+if [ "`echo ${NODE_HOST}`" == "" ]
 then
-  node="mogile-node"
+  NODE_HOST="mogile-node"
 fi
 
-port=$4
-if [ "`echo $port`" == "" ]
+if [ "`echo ${NODE_PORT}`" == "" ]
 then
-  port="7500"
+  NODE_PORT="7500"
 fi
 
 # Start mysql database
@@ -24,21 +22,21 @@ sleep 5
 
 sudo -u mogile mogilefsd --daemon -c /etc/mogilefs/mogilefsd.conf
 
-mogadm --trackers=127.0.0.1:7001 host add mogilestorage --ip=$node --port=$port --status=alive
+mogadm --trackers=127.0.0.1:7001 host add mogilestorage --ip=${NODE_HOST} --port=${NODE_PORT} --status=alive
 mogadm --trackers=127.0.0.1:7001 device add mogilestorage 1
 mogadm --trackers=127.0.0.1:7001 device add mogilestorage 2
 
-if [ "$domain" != "" ]
+if [ "`echo ${DOMAIN_NAME}`" != "" ]
 then
-  mogadm --trackers=127.0.0.1:7001 domain add $domain
+  mogadm --trackers=127.0.0.1:7001 domain add ${DOMAIN_NAME}
   mogadm class modify sbf default --replpolicy='MultipleDevices()'
 
   # Add all given classes
-  if [ "$classes" != "" ]
+  if [ "`echo ${CLASS_NAMES}`" != "" ]
   then
-    for class in $classes
+    for class in ${CLASS_NAMES}
     do
-      mogadm --trackers=127.0.0.1:7001 class add $domain $class --replpolicy="MultipleDevices()"
+      mogadm --trackers=127.0.0.1:7001 class add ${DOMAIN_NAME} $class --replpolicy="MultipleDevices()"
     done
   fi
 fi
